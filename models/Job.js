0861b8ded8e405d3f1db35c8b4ae4572069f74ba
ricksto87 <richard.stones@appsense.com>
2015-07-06 13:10:22 +0000

    added remote mongodb

---
+++ models/Job.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var jobSchema = mongoose.Schema({
+    title: { type: String },
+    description: {type: String }
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function() {
+    Job.find({}).exec(function(error, collection) {
+        if(collection.length === 0) {
+            Job.create({title:'Cook', description:'You will be making Bagels'});
+            Job.create({title:'Waiter', description:'You will be putting food on peoples tables'});
+            Job.create({title:'Programmer', description:'You will be mindlessly typing for hours'});
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -8,8 +9,24 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    })
+});
+
 app.get('*', function(req, res) {
     res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://rick:onelegged1@ds061208.mongolab.com:61208/jobfinder');
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+   console.log('connected to mongod succesfully!') ;
+   jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
